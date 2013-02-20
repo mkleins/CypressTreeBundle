040d1fbd926110915ba91122667700223005afff
mklein <mklein@wanadev.fr>
2013-02-20 18:09:04 +0100

    add search feature

--- Resources/public/jstree/jquery.jstree.js
+++ Resources/public/jstree/jquery.jstree.js
@@ -3407,9 +3407,11 @@
  * DOES NOT WORK WITH JSON PROGRESSIVE RENDER
  */
 (function ($) {
-	$.expr[':'].jstree_contains = function(a,i,m){
-		return (a.textContent || a.innerText || "").toLowerCase().indexOf(m[3].toLowerCase())>=0;
-	};
+	$.expr[':'].jstree_contains = $.expr.createPseudo(function(search) {
+      return function(a) {
+          return (a.textContent || a.innerText || "").toLowerCase().indexOf(search.toLowerCase()) >= 0;
+      };
+   });
 	$.expr[':'].jstree_title_contains = function(a,i,m) {
 		return (a.getAttribute("title") || "").toLowerCase().indexOf(m[3].toLowerCase())>=0;
 	};
